1089bd71e6847269c98180249f9eb2b238e0b9b0
yuvika-AG <yuvika926@gmail.com>
2026-03-14 00:03:53 +0530

    added a pattern1

---
+++ patterns/pattern1.cpp
@@ -0,0 +1,25 @@
+/*eg:
+  *
+  **
+  ***
+  ****
+*/
+#include <iostream>
+using namespace std;
+void pattern(int n){
+    
+    for(int i=0;i<n;i++){
+         for(int j=0;j<=i;j++){
+            cout<<"*";
+         }
+         cout<<"\n";
+    }
+}
+int main()
+{
+    int n;
+    cin>>n;
+    pattern(n);
+
+    return 0;
+}
